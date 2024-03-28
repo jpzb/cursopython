94fa9fd3688975d8c00cb6a8d7f24e78bbb4669f
Barcellos <jpzbarcellos@gmail.com>
2024-03-28 17:41:30 -0300

    Commit

--- Aulas(teorias)/modulo_random.py
+++ Aulas(teorias)/modulo_random.py
@@ -4,7 +4,6 @@ Módulo Random e o que são módulos?
 - Em Python, módulos nada mais são do que outros arquivos Python.
 
 Módulo Random -> Possui várias funções para geração de números pseudo-aleatórios.
-"""
 
 # OBS: Existem duas formas de se utilizar um módulo ou função deste
 
@@ -23,4 +22,47 @@ print(random.random())
 # Para utilizar a função random() do pacote random, nós colocamos o nome do pacote e o nome da função, separados
 # por ponto.
 
-# OBS: Não confunda a função random() com o pacote random.
+# OBS: Não confunda a função random() com o pacote random. Pode parecer confuso, mas a função random() é
+# apenas uma função dentro do módulo random.
+
+# Forma 2 - Importando uma função específica do módulo
+
+from random import random
+
+# No import acima, estamos falando: Do módulo random, importe a função random
+
+for i in range(10):
+    print(random())
+
+# uniform() -> Gerar um número real pseduo-aleatório entre os valores estabelecidos
+
+from random import uniform
+
+for i in range(10):
+    print(uniform(3, 7)) # 7 não é incluído.
+
+# randint() -> Gera valores pseudo-aleatórios entre os valores estabelecidos
+from random import randint
+
+for i in range(6):
+    print(randint(1, 61), end=", ")
+
+# choice() -> Mostra um valor aleatório entre um iterável.
+from random import choice
+
+jogadas = ["pedra", "papel", "tesoura"]
+
+print(choice(jogadas))
+
+print(choice("String"))
+
+# shuffle() -> Tem a função de embaralhar dados
+from random import shuffle
+
+cartas = ["K", "Q", 'J', 'A', 2, 3, 4, 5, 6, 7]
+
+print(cartas)
+shuffle(cartas)
+print(cartas)
+"""
+

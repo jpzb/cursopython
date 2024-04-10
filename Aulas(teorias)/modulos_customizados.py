da799606ff0925744c4083794c35bdbc799c735f
Barcellos <jpzbarcellos@gmail.com>
2024-04-09 21:24:52 -0300

    Commit

---
+++ Aulas(teorias)/modulos_customizados.py
@@ -0,0 +1,18 @@
+"""
+Módulos Customizados
+
+Como módulos Python nada mais são do que arquivos Python, então TODOS os arquivos que criamos
+neste curso são módulos Python prontos para serem utiliados.
+
+# Importando uma função específica do nosso módulo
+from funcoes_com_parametro import soma_impares
+
+print(soma_impares([1, 2, 3, 4, 45, 5, 5]))
+
+# Importando todo o módulo (Temos acesso a TODOS os elementos do módulo
+
+import funcoes_com_parametro as fcp
+
+# Acessando e imprimindo uma variável contida no módulo
+print(fcp.lista)
+"""
---
+++ Aulas(teorias)/modulos_externos.py
@@ -0,0 +1,18 @@
+"""
+Módulos Externos
+
+Utilizando o gerenciador de pacotes Python chamado de Pip - Python Installer Package
+
+Você pode conhecer todos os pacotes externos oficiais em: https://pypi.org
+
+colorama -> É utilizado para permitir impressão de textos coloridos no terminal
+
+Instalando um módulo:
+
+pip install nome-do-modulo
+
+from colorama import init, Fore
+
+init()
+print(Fore.MAGENTA + "String")
+"""

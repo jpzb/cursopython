e7756248ff6685afd71ee400a45f7d5829571383
Barcellos <jpzbarcellos@gmail.com>
2024-06-14 16:40:20 -0300

    Commit

---
+++ Aulas(teorias)/seek_e_cursor.py
@@ -0,0 +1,52 @@
+"""
+Seek e Cursurs
+
+seek() -> É utilizada para movimentar o cursor pelo arquivo.
+
+arquivo = open("texto.txt")
+print(arquivo.read())
+
+# seek() -> A função seek() é utilizada para movimentação do cursor pelo arquivo. Ela recebe um
+# parametro que indica onde queremos colocar o cursor.
+# Movimentando o cursor pelo arquivo com a função seek()
+arquivo.seek(0)
+
+print(arquivo.read())
+
+# readLine() -> Função que lê o arquivo linha a linha
+
+print(arquivo.readline())
+
+# readLines()
+
+print(arquivo.readlines())
+
+# OBS: Quando abrimos um arquivo com a função open() é criada uma conexão entre o arquivo
+no disco do computador e o nosso programa. Essa conexão é chamada de streaming. Ao finalizar
+os trabalhos com o arquivo, devemos fechar essa conexão. Para isso, utilizamos a função close()
+
+Passos para se trabalhar com um arquivo:
+
+1 - Abrir o arquivo;
+
+2 - Trabalhar o arquivo;
+
+3 - Fechar o arquivo;
+
+
+# 1 - Abrir o arquivo;
+arquivo = open("texto.txt")
+
+# 2 - Trabalhar o arquivo;
+print(arquivo.read())
+
+# 3 - Fechar o arquivo;
+arquivo.close()
+
+print(arquivo.closed)
+"""
+
+arquivo = open("texto.txt")
+
+# Com a função read(), podemos limitar a quantidade de caractéres a serem lidos no arquivo
+print(arquivo.read(50))
